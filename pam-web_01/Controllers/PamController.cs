45ac93cbf82383e06e55bbcab1fb01ef10e124e5
oussamatecnique <aouladlahcenoussama@gmail.com>
2020-02-17 15:55:57 +0100

    validé

--- pam-web_01/Controllers/PamController.cs
+++ pam-web_01/Controllers/PamController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Action_Model.Models;
+using pam_web_01.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,10 @@ namespace pam_web_01.Controllers
     public class PamController : Controller
     {
         // GET: Pam
-        public ActionResult Index()
+        [HttpGet]
+        public ViewResult Index(ApplicationModel application)
         {
-            return View();
+            return View(new IndexModel() { Application = application });
         }
         [HttpPost]
         public PartialViewResult FaireSimulation()
---
+++ pam-web_01/Models/IndexModel.cs
@@ -0,0 +1,30 @@
+﻿using Action_Model.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pam_web_01.Models
+{
+    [Bind(Exclude = "Application")]
+    public class IndexModel
+    {
+        // données de portée application
+        public ApplicationModel Application { get; set; }
+        // valeurs postées
+        [Required]
+        [Display(Name = "Employé")]
+        public string SS { get; set; }
+        [Required]
+        [Display(Name = "Heures travaillées")]
+        [UIHint("Decimal")]
+        [Range(0,400,ErrorMessage ="entrer un nombre entre 0 et 400")]
+        public double HeuresTravaillées { get; set; }
+        [Required]
+        [Display(Name = "Jours travaillés")]
+        [Range(0,31, ErrorMessage = "entrer un nombre entre 0 et 31")]
+        public double JoursTravaillés { get; set; }
+    }
+}
